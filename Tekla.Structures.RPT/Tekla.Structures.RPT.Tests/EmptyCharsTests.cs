a411e1735baecd1eaeb860885d650f7c6161ed85
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-03-01 17:58:32 +0100

    Added EmptyChars class to inteligent remove empty chars

---
+++ Tekla.Structures.RPT/Tekla.Structures.RPT.Tests/EmptyCharsTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Tekla.Structures.RPT.Tests
+{
+    public class EmptyCharsTests
+    {
+        private string GetTestDir()
+        {
+            return Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory));
+        }
+
+        //We have to remove blank and empty chars from text but not from string values
+
+        [Test]
+        public void InteligentEmptyCharsRemove()
+        {
+            var fileName = Path.Combine(GetTestDir(), "emptyCharsCase1.txt");
+            var text = File.ReadAllText(fileName);
+            var ech = new EmptyChars();
+
+            var result = ech.Remove(text);
+
+            Console.WriteLine(result);
+
+            Assert.IsTrue(result.Contains("template_1	ff	asdfas erwtcxb      cxb cxvb cv   cxb sdsdg sd"));
+            Assert.IsTrue(result.Contains(";pageheader{name=\"PAGEHEADER one \";height=6;outputpolicy=NONE;valuef"));
+        }
+    }
+}
---
+++ Tekla.Structures.RPT/Tekla.Structures.RPT/EmptyChars.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Tekla.Structures.RPT
+{
+    internal class EmptyChars
+    {
+        private string text;
+        private int quoteLevel;
+
+        private void Reset()
+        {
+            text = string.Empty;
+            quoteLevel = 0;
+        }
+
+        public string Remove(string inputText)
+        {
+            Reset();
+            var outputText = new StringBuilder(inputText.Length);
+            outputText.Append(inputText[0]);
+            this.text = inputText;
+
+            for (int i = 1; i < inputText.Length; i++)
+            {
+                CalcuateLevels(i);
+
+                if (quoteLevel.Equals(0))
+                {
+                    if (!char.IsWhiteSpace(inputText[i]))
+                        outputText.Append(inputText[i]);
+                }
+                else
+                    outputText.Append(inputText[i]);
+            }
+            
+            return outputText.ToString();
+        }
+
+        private void CalcuateLevels(int index)
+        {
+            var currentChar = text[index];
+            var previousChar = text[index - 1];
+
+            if (currentChar.Equals('\"') & !previousChar.Equals('\\') & quoteLevel.Equals(0))
+                quoteLevel++;
+            else if (currentChar.Equals('\"') & !previousChar.Equals('\\') & quoteLevel.Equals(1))
+                quoteLevel--;
+        }
+    }
+}
